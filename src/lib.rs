b1c026f2492133c30d68ba3d5fd30284067f39a2
Stjepan Glavina <stjepang@gmail.com>
2020-05-26 10:35:12 +0200

    Remove confusing wording

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,6 @@
 //! sacrificing performance. This is done by forcing a fair lock whenever a lock operation is
 //! starved for longer than 0.5 milliseconds.
 //!
-//! Each instance of [`Mutex`] requires 2 words of storage in addition to inner data. Some
-//! additional metadata gets allocated on the heap on first case of contention.
-//!
 //! # Examples
 //!
 //! ```
